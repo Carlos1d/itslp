5d712f0ebefc69cbf49d1a08dca7135545e7b815
Carlos1d <ing.carl0s.ramirez9@gmail.com>
2020-12-01 21:08:47 -0600

    Base de datos

---
+++ database/seeders/LibroSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LibroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('libro')->insert([
+       	'nombre'=>'Servidores',
+       	'autor'=>'Luis ',
+       ]);
+       DB::table('libro')->insert([
+       	'nombre'=>'Hola mundo',
+       	'autor'=>'Death Silent ',
+       ]);
+   }
+}
