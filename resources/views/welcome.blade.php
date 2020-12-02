b559675bc76254074060ac57362fbe29d09f3e9d
Carlos1d <ing.carl0s.ramirez9@gmail.com>
2020-12-01 23:21:16 -0600

    Mi nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <h1>Carlos Ivan Ramirez Cervantes</h1><br>
+        <h1>Carlos Ivan Ramirez Cervantes</h1><br/>
         
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
